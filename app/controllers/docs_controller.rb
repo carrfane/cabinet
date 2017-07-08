e786a4c43f5409d918a4491b2987dd4b4c78e992
Hector Carrillo <carrfane@gmail.com>
2017-07-08 17:43:15 -0400

    crud added

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -1,13 +1,20 @@
 class DocsController < ApplicationController
-
+  before_action :find_doc, only: [:show, :edit, :update, :destroy]
   def index
-
+    @docs = Doc.all.order('created_at DESC')
   end
 
   def create
+  	@doc = Doc.new(doc_params)
+  	if @doc.save
+  		redirect_to @doc
+  	else
+  		render 'new'
+  	end
   end
 
   def new
+  	@doc = Doc.new
   end
 
   def edit
@@ -17,16 +24,28 @@ class DocsController < ApplicationController
   end
 
   def update
+  	if @doc.update(doc_params)
+  		redirect_to @doc
+  	else
+  		render 'edit'
+  	end
   end
 
   def destroy
+  	if @doc.destroy
+  		redirect_to docs_path
+  	else
+  		doc_path
+  	end
   end
 
   private
 
     def find_doc
+    	@doc = Doc.find(params[:id])
     end
 
     def doc_params
+    	params.require(:doc).permit(:title, :content)
     end
 end
